1c89cc942bf111933ab6c613ba221e70da3561f3
lilDorito <mihailfedorec24@gmail.com>
2026-03-09 18:43:48 +0200

    Update telegram_scraper.py
    path config

--- scripts/telegram/telegram_scraper.py
+++ scripts/telegram/telegram_scraper.py
@@ -1,33 +1,35 @@
 import pandas as pd
 from datetime import datetime, timezone
 import os
+import sys
 import asyncio
 from telethon import TelegramClient
 from dotenv import load_dotenv
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+OUTPUT_FILE = os.path.join(ROOT, "datasets", "telegram", "telegram_data.csv")
+SESSION = os.path.join(ROOT, "scripts", "telegram", "session")
+
+sys.path.append(os.path.dirname(__file__))
 from text_cleaner import clean_text as clean
 from event_detector import detect_events
 
-load_dotenv()
-
+load_dotenv(os.path.join(ROOT, ".env"))
 API_ID = int(os.getenv("TELEGRAM_API_ID"))
 API_HASH = os.getenv("TELEGRAM_API_HASH")
 
-CHANNELS = [
+CHANNELS   = [
     "radar_raketa", "Ukraine_UA_24_7", "air_alert_telegram", "alarmua",
     "ukrpravda_news", "pravda_ukraineee", "suspilnenews", "uniannet",
     "hromadske_ua", "war_monitor", "nexta_live", "GeneralStaffZSU", "kpszsu"
 ]
-
 SINCE_DATE = datetime(2022, 2, 24, tzinfo=timezone.utc)
-
-OUTPUT_FILE = "telegram_data.csv"
 
 async def main():
     print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M')}] Start collecting...")
-
     data = []
 
-    async with TelegramClient("session", API_ID, API_HASH) as client:
+    async with TelegramClient(SESSION, API_ID, API_HASH) as client:
         for channel in CHANNELS:
             try:
                 async for message in client.iter_messages(channel):
@@ -47,7 +49,7 @@ async def main():
                         "events": ",".join(sorted(events))
                     })
             except Exception as e:
-                print(f"Error in {channel}: {e}")
+                print(f"[!] Error in {channel}: {e}")
 
     if not data:
         print("Nothing collected.")
@@ -56,9 +58,10 @@ async def main():
     df = pd.DataFrame(data)
     df = df.drop_duplicates(subset=["message_id"])
     df = df.sort_values("message_date", ascending=False)
+    os.makedirs(os.path.dirname(OUTPUT_FILE), exist_ok=True)
     df.to_csv(OUTPUT_FILE, index=False, encoding="utf-8")
+    print(f"Collected {len(df)} posts -> {OUTPUT_FILE}")
 
-    print(f"Collected {len(df)} posts. Saved to {OUTPUT_FILE}")
 
 if __name__ == "__main__":
     asyncio.run(main())
