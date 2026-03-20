76183d43608e10b0746916d8bf0a4fa6f00209a9
lilDorito <mihailfedorec24@gmail.com>
2026-03-20 09:48:21 +0000

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,8 @@ mkdir -p datasets/{alarms,isw,telegram,reddit,weather}
 mkdir -p logs/{alarms,isw,telegram,reddit,weather}
 mkdir -p notebooks/{}
 mkdir -p scratch/{}
+mkdir -p predictions/{}
+mkdir -p plots/{}
+mkdir -p models/{}
 cp .env.example .env
 echo "Done. Fill in your .env file."
